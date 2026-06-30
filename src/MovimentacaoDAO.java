1f2ac605794d5752e291d3a525000f739755edc5
ArtMainardi <iensenmainardi@gmail.com>
2026-06-30 20:20:24 -0300

    fix: corrige chamadas de getters

--- src/MovimentacaoDAO.java
+++ src/MovimentacaoDAO.java
@@ -14,11 +14,11 @@ public class MovimentacaoDAO {
         String sql = "INSERT INTO Movimentacao (qtd_movimentacao, data_movimentacao, id_funcionario, id_produto, id_tipo) VALUES (?, ?, ?, ?, ?)";
         
         try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            stmt.setInt(1, m.getQtdMovimentacao());
-            stmt.setObject(2, m.getDataMovimentacao());
-            stmt.setInt(3, m.getFuncionario().getId_funcionario());     
-            stmt.setInt(4, m.getProduto().getId_produto());          
-            stmt.setInt(5, m.getTipo().getId_tipo());              
+            stmt.setInt(1, m.getQtd_movimentacao());
+            stmt.setObject(2, m.getData_movimentacao());
+            stmt.setInt(3, m.getFuncionario().getId_funcionario());
+            stmt.setInt(4, m.getProduto().getId_produto());
+            stmt.setInt(5, m.getTipo().getId_tipo());
             
             stmt.executeUpdate();
         }
