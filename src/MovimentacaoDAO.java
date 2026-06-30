8766ec2700f8dc0bb219c2dbd92ca7d88dde122a
ArtMainardi <iensenmainardi@gmail.com>
2026-06-30 19:42:38 -0300

    fix: corrige nome da tabela na String sql

--- src/MovimentacaoDAO.java
+++ src/MovimentacaoDAO.java
@@ -10,7 +10,7 @@ public class MovimentacaoDAO {
     }
 
     public void salvar(MovimentacaoModel m) throws SQLException {
-        String sql = "INSERT INTO movimentacao (qtd_movimentacao, data_movimentacao, id_funcionario, id_produto, id_tipo) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO Movimentacao (qtd_movimentacao, data_movimentacao, id_funcionario, id_produto, id_tipo) VALUES (?, ?, ?, ?, ?)";
         
         try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setInt(1, m.getQtdMovimentacao());
