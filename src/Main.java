b80d6c26fc2799d0d2a80e99165147bc74ec89ed
ArtMainardi <iensenmainardi@gmail.com>
2026-07-03 17:51:27 -0300

    feat: adiciona procedimento de login

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+        login();
+    }
+
+    // Procedimento para tela de login (BETA):
+    public static void login(){
+        String emailCadastrado = "jdbc@gmail.com";
+        String senhaCadastrada = "123";
+
+        System.out.println("Digite seu email: ");
+        String email = sc.nextLine();
+        System.out.println("Digite sua senha: ");
+        String senha = sc.nextLine();
+
+        if(email.equals(emailCadastrado)){
+            if(senha.equals(senhaCadastrada)){
+                System.out.println("Login efetuado com sucesso!");
+            }else{
+                System.out.println("ERRO: senha incorreta!");
+            }
+        } else{
+            System.out.println("ERRO: email inválido!");
+        }
     }
 }
