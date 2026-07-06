17080ced3c6f1e6a5ad14bbb341287017a99f4b6
ArtMainardi <iensenmainardi@gmail.com>
2026-07-06 16:05:57 -0300

    refac: normalização de dados

--- src/ProdutoDAO.java
+++ src/ProdutoDAO.java
@@ -1,39 +1,18 @@
 import connection.Conexao;
-import model.ProdutoModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import model.ProdutoModel;
 
 
 public class ProdutoDAO {
     Conexao conexao = new Conexao();
 
-    public ArrayList <ProdutoModel> listar() throws SQLException{
-        ArrayList<ProdutoModel> lista = new ArrayList<>();
-
-        String sql = "SELECT * FROM Produto";
-
-        Connection conn = conexao.conectar();
-        Statement stmt = conn.createStatement();
-        ResultSet resultado = stmt.executeQuery(sql);
-
-        while(resultado.next()){
-            int id_produto = resultado.getInt("id_produto");
-            String nome_produto = resultado.getString("nome_produto");
-            String descricao_produto = resultado.getString("descricao_produto");
-            int qtd_produto = resultado.getInt("qtd_produto");
-            int qtd_minima = resultado.getInt("qtd_minima");
-            boolean ativo = resultado.getBoolean("ativo");
-
-            lista.add(new ProdutoModel(id_produto,nome_produto, descricao_produto, qtd_produto, qtd_minima, ativo));
-        }
-        return lista;
-    }
-
-    public void inserir(ProdutoModel p) throws SQLException{
+    // CREATE:
+    public void create(ProdutoModel p) throws SQLException{
         String sql = "INSERT INTO Produto(nome_produto, descricao_produto, qtd_produto, qtd_minima, ativo) VALUES (?,?,?,?,?)";
 
         try(Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -48,11 +27,31 @@ public class ProdutoDAO {
         }
     }
 
-    public ProdutoModel buscar (int id) throws SQLException{
+    // READ:
+    public ArrayList <ProdutoModel> read() throws SQLException{
+        ArrayList<ProdutoModel> lista = new ArrayList<>();
+        String sql = "SELECT * FROM Produto";
+
+        try(Connection conn = conexao.conectar(); Statement stmt = conn.createStatement(); ResultSet resultado = stmt.executeQuery(sql);){
+            while(resultado.next()){
+                int id_produto = resultado.getInt("id_produto");
+                String nome_produto = resultado.getString("nome_produto");
+                String descricao_produto = resultado.getString("descricao_produto");
+                int qtd_produto = resultado.getInt("qtd_produto");
+                int qtd_minima = resultado.getInt("qtd_minima");
+                boolean ativo = resultado.getBoolean("ativo");
+
+                lista.add(new ProdutoModel(id_produto,nome_produto, descricao_produto, qtd_produto, qtd_minima, ativo));
+            }
+            return lista;
+        }
+    }
+
+    // READ (ID):
+    public ProdutoModel readId(int id) throws SQLException{
         String sql = "SELECT * FROM Produto WHERE id_produto = ?";
 
         try(Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)){
-            
             stmt.setInt(1, id);
 
             try(ResultSet resultado = stmt.executeQuery();){
@@ -69,10 +68,11 @@ public class ProdutoDAO {
                 }
             }
         }
-                return null;
+        return null;
     }
 
-    public boolean atualizar(ProdutoModel produtoModificado) throws SQLException{
+    // UPDATE:
+    public boolean update(ProdutoModel produtoModificado) throws SQLException{
         String sql = "UPDATE Produto SET nome_produto = ?, descricao_produto = ?, qtd_produto = ?, qtd_minima = ?, ativo = ? WHERE id_produto = ?";
 
         try(Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -81,7 +81,6 @@ public class ProdutoDAO {
             stmt.setInt(3, produtoModificado.getQtd_produto());
             stmt.setInt(4, produtoModificado.getQtd_minima());
             stmt.setBoolean(5, produtoModificado.isAtivo());
-
             int linhasAfetadas = stmt.executeUpdate();
 
             if(linhasAfetadas > 0){
@@ -92,12 +91,12 @@ public class ProdutoDAO {
         }
     }
 
+    // DELETE (DESATIVAR):
     public boolean desativar(int id) throws SQLException{
         String sql = "UPDATE Produto SET ativo = false WHERE id_produto = ?";
 
         try(Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)){
-            stmt.setString(1, id);
-            
+            stmt.setInt(1, id);
             int linhasAfetadas = stmt.executeUpdate();
 
             if(linhasAfetadas > 0){
@@ -108,12 +107,12 @@ public class ProdutoDAO {
         }
     }
 
+    // DELETE (ATIVAR):
     public boolean ativar (int id) throws SQLException {
         String sql = "UPDATE Produto SET ativo = true WHERE id_produto = ?";
 
         try(Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)){
-            stmt.setString(1, id);
-
+            stmt.setInt(1, id);
             int linhasAfetadas = stmt.executeUpdate();
 
             if(linhasAfetadas > 0){
