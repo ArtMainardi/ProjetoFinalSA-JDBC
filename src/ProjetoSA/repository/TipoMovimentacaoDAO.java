3fd69ee9f4c923de1bc1d565a167f80f49ee8eda
ArtMainardi <iensenmainardi@gmail.com>
2026-07-07 16:36:18 -0300

    feat: adiciona 'salvar()'

--- src/ProjetoSA/repository/TipoMovimentacaoDAO.java
+++ src/ProjetoSA/repository/TipoMovimentacaoDAO.java
@@ -1,5 +1,6 @@
 package ProjetoSA.repository;
 import ProjetoSA.connection.Conexao;
+import ProjetoSA.model.TipoMovimentacaoModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,7 +8,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import ProjetoSA.model.TipoMovimentacaoModel;
 
 public class TipoMovimentacaoDAO {
     Conexao conexao = new Conexao();
---
+++ src/ProjetoSA/service/TipoMovimentaçãoService.java
@@ -0,0 +1,30 @@
+package ProjetoSA.service;
+
+import ProjetoSA.model.TipoMovimentacaoModel;
+import ProjetoSA.repository.TipoMovimentacaoDAO;
+
+public class TipoMovimentaçãoService {
+    // Cria objeto DAO para podermos usar seus métodos:
+    private TipoMovimentacaoDAO repository = new TipoMovimentacaoDAO();
+
+    // Salvar:
+    public String salvar(TipoMovimentacaoModel objeto){
+        String message;
+        try {
+            // Verifica o atributo 'tipo':
+            if(objeto.getTipo().trim().isEmpty()){
+                throw new Exception("ERRO: nenhum dado do objeto pode ser vazio!");
+            }
+            // Verifica o retorno booleano do 'crate()':
+            if(!repository.create(objeto)){
+                throw new Exception("ERRO: algo deu de errado");
+            }
+            // Define a mensagem de retorno:
+            message = "Tipo de movimentação salvo com sucesso!";
+        } catch (Exception e) {
+            message = e.getMessage();
+        }
+        // Retorna a mensagem:
+        return message;
+    }
+}
