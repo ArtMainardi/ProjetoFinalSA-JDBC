86a45757f93bf1fad4d7d688aa3089d2f5767ffe
LEONARDO MAFIOLETTI <leonardo_mafioletti@estudante.sesisenai.org.br>
2026-07-07 21:00:12 -0300

    fix: corrige classe DAO

--- src/ProjetoSA/repository/MovimentacaoDAO.java
+++ src/ProjetoSA/repository/MovimentacaoDAO.java
@@ -1,4 +1,9 @@
 package ProjetoSA.repository;
+import ProjetoSA.connection.Conexao;
+import ProjetoSA.model.FuncionarioModel;
+import ProjetoSA.model.MovimentacaoModel;
+import ProjetoSA.model.ProdutoModel;
+import ProjetoSA.model.TipoMovimentacaoModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,23 +11,15 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import ProjetoSA.model.FuncionarioModel;
-import ProjetoSA.model.MovimentacaoModel;
-import ProjetoSA.model.ProdutoModel;
-import ProjetoSA.model.TipoMovimentacaoModel;
 
 public class MovimentacaoDAO {
-    private Connection conexao;
-
-    public MovimentacaoDAO(Connection conexao) {
-        this.conexao = conexao;
-    }
+    private Conexao conexao = new Conexao();
 
     // 1. CREATE 
     public void salvar(MovimentacaoModel m) throws SQLException {
         String sql = "INSERT INTO Movimentacao (qtd_movimentacao, data_movimentacao, id_funcionario, id_produto, id_tipo) VALUES (?, ?, ?, ?, ?)";
         
-        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try (Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql);) {
             stmt.setInt(1, m.getQtd_movimentacao());
             stmt.setObject(2, m.getData_movimentacao()); // LocalDate funciona muito bem com setObject
             stmt.setInt(3, m.getFuncionario().getId_funcionario());
@@ -38,7 +35,7 @@ public class MovimentacaoDAO {
         String sql = "SELECT id_movimentacao, qtd_movimentacao, data_movimentacao, id_funcionario, id_produto, id_tipo FROM Movimentacao";
         List<MovimentacaoModel> movimentacoes = new ArrayList<>();
 
-        try (PreparedStatement stmt = conexao.prepareStatement(sql);
+        try (Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
             
             while (rs.next()) {
@@ -66,7 +63,7 @@ public class MovimentacaoDAO {
     public MovimentacaoModel buscarPorId(int idBusca) throws SQLException {
         String sql = "SELECT id_movimentacao, qtd_movimentacao, data_movimentacao, id_funcionario, id_produto, id_tipo FROM Movimentacao WHERE id_movimentacao = ?";
         
-        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try (Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql);) {
             stmt.setInt(1, idBusca);
             
             try (ResultSet rs = stmt.executeQuery()) {
@@ -95,7 +92,7 @@ public class MovimentacaoDAO {
     public void atualizar(MovimentacaoModel m) throws SQLException {
         String sql = "UPDATE Movimentacao SET qtd_movimentacao = ?, data_movimentacao = ?, id_funcionario = ?, id_produto = ?, id_tipo = ? WHERE id_movimentacao = ?";
         
-        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try (Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql);) {
             stmt.setInt(1, m.getQtd_movimentacao());
             stmt.setObject(2, m.getData_movimentacao());
             stmt.setInt(3, m.getFuncionario().getId_funcionario());
@@ -111,7 +108,7 @@ public class MovimentacaoDAO {
     public void deletar(int id) throws SQLException {
         String sql = "DELETE FROM Movimentacao WHERE id_movimentacao = ?";
         
-        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try (Connection conn = conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql);) {
             stmt.setInt(1, id);
             
             stmt.executeUpdate();
