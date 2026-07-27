6ba667f9a76e07178e7a4d42d7e53b0fdbd711ff
ArtMainardi <iensenmainardi@gmail.com>
2026-07-27 18:56:24 -0300

    refac: ajusta e adiciona comentários

--- src/ProjetoSA/service/ProdutoService.java
+++ src/ProjetoSA/service/ProdutoService.java
@@ -7,94 +7,94 @@ import ProjetoSA.model.ProdutoModel;
 import ProjetoSA.repository.ProdutoDAO;
 
 public class ProdutoService {
-
     private ProdutoDAO repository = new ProdutoDAO();
 
-    // salvar
-        public void salvar(ProdutoModel produto) throws SQLException{
-            if(produto.getNome_produto().trim().isEmpty()){
-                throw new RuntimeException("ERRO: Nenhum dado do produto pode ser vazio!");
-            }
-
-            if(produto.getQtd_produto() < 0){
-                throw new RuntimeException("ERRO: quantidade do produto não pode ser negativa!");
-            }
-            if(produto.getQtd_minima() < 0){
-                throw new RuntimeException("ERRO: quantidade mínima do produto não pode ser negativa!");
-            }
-            repository.create(produto);
+    // Salvar
+    public void salvar(ProdutoModel produto) throws SQLException{
+        // Verifica 'nome' do produto:
+        if(produto.getNome_produto().trim().isEmpty()){
+            throw new RuntimeException("ERRO: Nenhum dado do produto pode ser vazio!");
         }
-
-     // Listar
-        public List<ProdutoModel> listar() throws SQLException{
-            List<ProdutoModel> lista = repository.read();
-
-            if(lista.isEmpty()){
-                throw new RuntimeException("Nenhum produto salvo no banco de dados!");
-            }
-            return lista;
+        // Verifica 'quantidade':
+        if(produto.getQtd_produto() < 0){
+            throw new RuntimeException("ERRO: quantidade do produto não pode ser negativa!");
         }
+        // Verifica 'quantidade mínima':
+        if(produto.getQtd_minima() < 0){
+            throw new RuntimeException("ERRO: quantidade mínima do produto não pode ser negativa!");
+        }
+        // Manda requisição para o repository:
+        repository.create(produto);
+    }
+
+    // Listar
+    public List<ProdutoModel> listar() throws SQLException{
+        List<ProdutoModel> lista = repository.read();
+        // Verifica se encontrou algum dado:
+        if(lista.isEmpty()){
+            throw new RuntimeException("Nenhum produto salvo no banco de dados!");
+        }
+        // Retorna a lista:
+        return lista;
+    }
 
     // Buscar id
-        public ProdutoModel buscarID(int id_produto) throws SQLException{
-            if(id_produto < 0){
-                throw new RuntimeException("ERRO: produto com ID negativo");
-            }
-
-            ProdutoModel produto = repository.readId(id_produto);
-
-            if(produto == null){
-                throw new RuntimeException("ERRO: produto com esse ID não encontrado!");
-            }
-            return produto;
+    public ProdutoModel buscarID(int id_produto) throws SQLException{
+        // Verifica integridade do ID:
+        if(id_produto < 0){
+            throw new RuntimeException("ERRO: ID informado inválido!");
         }
+        // Verifica se encontrou algum dado:
+        ProdutoModel produto = repository.readId(id_produto);
+        if(produto == null){
+            throw new RuntimeException("ERRO: produto com esse ID não encontrado!");
+        }
+        // Retorna o produto:
+        return produto;
+    }
 
      // Atualizar
-        public void atualizar(ProdutoModel modifiedProduto) throws SQLException{
-            if(modifiedProduto.getNome_produto().trim().isEmpty()){
-                throw new RuntimeException("ERRO: nenhum dado do produto pode ser vazio!");
-            }
-
-            if(modifiedProduto.getQtd_produto() < 0){
-                throw new RuntimeException("ERRO: quantidade do produto não pode ser negativa!");
-            }
-            if(modifiedProduto.getQtd_minima() < 0){
-                throw new RuntimeException("ERRO: quantidade mínima do produto não pode ser negativa!");
-            }
-
-            ProdutoModel produto = repository.readId(modifiedProduto.getId_produto());
-
-            if(produto == null){
-                throw new RuntimeException("ERRO: produto com esse ID não encontrado!");
-            }
-            
-            if(!repository.update(modifiedProduto)){
-                throw new RuntimeException("ERRO: algo deu errado!");
-            }
-
+    public void atualizar(ProdutoModel modifiedProduto) throws SQLException{
+        //  Verifica 'nome' do produto:
+        if(modifiedProduto.getNome_produto().trim().isEmpty()){
+            throw new RuntimeException("ERRO: nenhum dado do produto pode ser vazio!");
         }
+        // Verifica 'quantidade':
+        if(modifiedProduto.getQtd_produto() < 0){
+            throw new RuntimeException("ERRO: quantidade do produto não pode ser negativa!");
+        }
+        // Verifica 'quantidade mínima':
+        if(modifiedProduto.getQtd_minima() < 0){
+            throw new RuntimeException("ERRO: quantidade mínima do produto não pode ser negativa!");
+        }
+        // Verifica se encontrou algum dado:
+        if(repository.readId(modifiedProduto.getId_produto()) == null){
+            throw new RuntimeException("ERRO: produto com esse ID não encontrado!");
+        }
+        // Manda a rquisição para o repository:
+        repository.update(modifiedProduto);
+    }
 
     // Desativar ou Ativar
-        public boolean desativarOuAtivar(int id, boolean estado) throws SQLException{
-            if(id < 0){
-                throw new RuntimeException("ERRO: produto com ID negativo!");
-            }
-
-            ProdutoModel produto = repository.readId(id);
-            if(produto == null){
-                throw new RuntimeException("ERRO: produto com esse ID não encontrado!");
-            }
-
-            if(produto.isAtivo() == estado){
-                throw new RuntimeException("ERRO: o objeto com esse ID já está " + (estado ? "ativo" : "desativo") + "!");
-            }
-            boolean sucesso = estado ? repository.ativar(id) : repository.desativar(id);
-
-            if(!sucesso){
-                throw new RuntimeException("ERRO: algo deu errado!");
-            }
-            return estado;
+    public void desativarOuAtivar(int id, boolean estado) throws SQLException{
+        // Verifica integridade do ID:
+        if(id < 0){
+            throw new RuntimeException("ERRO: produto com ID negativo!");
         }
-  
-
+        // Verifica se encontrou algum dado:
+        ProdutoModel produto = repository.readId(id);
+        if(produto == null){
+            throw new RuntimeException("ERRO: produto com esse ID não encontrado!");
+        }
+        // Verifica se o dado já não possui o estado desejado:
+        if(produto.isAtivo() == estado){
+            throw new RuntimeException("ERRO: o objeto com esse ID já está " + (estado ? "ativo" : "desativo") + "!");
+        }
+        // Manda a requisição para o repository:
+        if(estado){
+            repository.ativar(id);
+        } else{
+            repository.desativar(id);
+        }
+    }
 }
