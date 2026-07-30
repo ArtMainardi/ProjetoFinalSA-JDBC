19a9b411eb1d09a6440b816917627a9f971126ae
Mafioletti <leomafioletti@gmail.com>
2026-07-30 15:52:08 -0300

    feat: inicia sub-menu dos produtos

--- src/ProjetoSA/util/ProdutoMain.java
+++ src/ProjetoSA/util/ProdutoMain.java
@@ -1,5 +1,15 @@
 package ProjetoSA.util;
 
+import ProjetoSA.service.ProdutoService;
+import java.util.Scanner;
+
 public class ProdutoMain {
-    
+    static Style sty;
+    static Scanner sc;
+    static ProdutoService service = new ProdutoService();
+
+    public static void main(Style style, Scanner scanner){
+        sty = style;
+        sc = scanner;
+    }
 }
