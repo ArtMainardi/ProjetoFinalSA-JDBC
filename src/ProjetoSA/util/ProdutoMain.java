ea63277e683ad9b15b3a289d870246a37ac19af9
Mafioletti <leomafioletti@gmail.com>
2026-07-30 16:27:08 -0300

    feat: adiciona mais funcionalidades ao menu de produtos

--- src/ProjetoSA/util/ProdutoMain.java
+++ src/ProjetoSA/util/ProdutoMain.java
@@ -1,7 +1,10 @@
 package ProjetoSA.util;
 
+import ProjetoSA.Main;
 import ProjetoSA.service.ProdutoService;
 import java.util.Scanner;
+
+import ProjetoSA.Main;
 
 public class ProdutoMain {
     static Style sty;
@@ -9,7 +12,38 @@ public class ProdutoMain {
     static ProdutoService service = new ProdutoService();
 
     public static void main(Style style, Scanner scanner){
+        //definição de variáveis
         sty = style;
         sc = scanner;
+
+        Main.clear();
+        //menu de opções
+        int opcao = 0;
+        do { 
+            try {
+                sty.titulo("Produtos");
+                System.out.println("Digite uma opção: \n"
+                                + "1 - Visualizar Histórico Geral \n"
+                                + "2 - Cadastrar Produto \n"
+                                + "3 - Listar Produtos \n"
+                                + "4 - Atualizar Produto \n"
+                                + "5 - Deletar Produto \n"
+                                + "6 - Sair");
+                opcao = Integer.parseInt(sc.nextLine().trim());
+                switch (opcao) {
+                    case 1:
+                        mostrarHistorico();
+                        break;
+                    default:
+                        throw new Exception("ERRO: opção digitada inválida!");
+                }
+            } catch (Exception e) {
+                sty.quadro(e.getMessage());
+            }
+            Main.continuar();
+            Main.clear();
+        } while (opcao != 6);
     }
+    //método para printar o histórico de produtos:
+    
 }
