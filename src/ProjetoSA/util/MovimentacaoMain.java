83ff38169657093aa9cfb518d3301dc1c095b771
Mafioletti <leomafioletti@gmail.com>
2026-08-04 16:07:28 -0300

    feat: adiciona função mostrarDesativados na classe MovimentacaoMain

--- src/ProjetoSA/util/MovimentacaoMain.java
+++ src/ProjetoSA/util/MovimentacaoMain.java
@@ -1,13 +1,12 @@
 package ProjetoSA.util;
 
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Scanner;
-
 import ProjetoSA.Main;
 import ProjetoSA.model.FuncionarioModel;
 import ProjetoSA.model.MovimentacaoModel;
 import ProjetoSA.service.MovimentacaoService;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
 
 public class MovimentacaoMain {
     static Style sty;
@@ -26,9 +25,10 @@ public class MovimentacaoMain {
             try{
                 sty.titulo("Movimentações");
                 System.out.println("Digite uma opção: \n"
-                                + "1- Visualizar Histórico Geral \n"
-                                + "2- Registrar Movimentação \n"
-                                + "3- Detalhes de uma Movimentação \n"
+                                + "1 - Visualizar Histórico Geral \n"
+                                + "2 - Registrar Movimentação \n"
+                                + "3 - Detalhes de uma Movimentação \n"
+                                + "4 - Mostrar movimentações desativadas"
                                 + "0- Voltar");
                 option = Integer.parseInt(sc.nextLine().trim());
                 switch (option) {
@@ -42,6 +42,10 @@ public class MovimentacaoMain {
                         break;
                     case 3:
                         detalhesMovimentacao(usuarioAtual);
+                        break;
+                    case 4:
+                        mostrarDesativados();
+                        Main.continuar();
                         break;
                     case 0:
                         break;
@@ -257,5 +261,21 @@ public class MovimentacaoMain {
         }
         Main.continuar();
         Main.clear();
+    }
+    public static void mostrarDesativados(){
+        try{
+            Main.clear();
+            sty.titulo("Movimentações Desativadas");
+            sty.quadro("ID   |     Quantidade     |   Data   |   Funcionario   |   Produto   |   Tipo");
+
+            // Faz a requisição:
+            List<MovimentacaoModel> movimentacoes = service.listarDesativados();
+            // Lista os produtos na tela:
+            for(MovimentacaoModel m : movimentacoes){
+                sty.quadro(m.mostrarDados());
+            }
+        } catch(Exception e){
+            sty.quadro(e.getMessage());
+        }
     }
 }
